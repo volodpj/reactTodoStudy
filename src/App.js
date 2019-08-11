3750591477162dd0a0ec93c25d7dd7730f912139
volodpj <volod.af@gmail.com>
2019-08-11 22:17:11 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import todos from './api/todos';
 import './App.css';
 
 function App() {
+  console.log(todos)
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
+      
     </div>
   );
 }
---
+++ src/api/todos.js
@@ -0,0 +1,10 @@
+const todos = [
+    {id: 1, title: 'clone', done: false},
+    {id: 2, title: 'clone', done: false},
+    {id: 3, title: 'clone', done: false},
+    {id: 4, title: 'clone', done: false},
+    {id: 5, title: 'clone', done: false},
+    {id: 6, title: 'clone', done: false},
+]
+
+export default todos
