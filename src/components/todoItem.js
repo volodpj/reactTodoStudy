e64dc57b6f0ec91686a6cc9b0693d04ef982cd38
volodpj <volod.af@gmail.com>
2019-09-06 18:02:16 +0300

    v3

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -2,29 +2,44 @@ import React from 'react';
 
 class TodoItem extends React.Component{
     state = {
+        todos1: this.props.todos,
         selectedTodoId: null,
     }
 
     changeTodo = (itemId) => {
+    
         this.setState({
             selectedTodoId: itemId,
         })
+    };
+
+    changeTodoTitle = (event) => {
+        console.log(event.target.value)
+       
     }
     render(){
-      
+        // const todos = [...this.props.todos]
+       
         return(
             <div>
                 <h1>title {this.state.selectedTodoId}</h1>
                 <ul>{
-                    this.props.todos.map((item) => {
+                    this.state.todos1.map((item) => {
                         return (
                             <li key={item.id} 
                                 onClick={() => {this.changeTodo(item.id)}}
                                 className={this.state.selectedTodoId === item.id ? 'active' : ''}
                             >
                                 <span>{item.id}</span>
-                                <span>{item.title}</span>
-                                <input type="checkbox" defaultChecked={item.done}></input>
+                                <span>
+                                    
+                                    <input 
+                                        type="text" 
+                                        value={item.title} 
+                                        onChange={(event) => {this.changeTodoTitle(event)}}
+                                    ></input>
+                                </span>
+                                <input type="checkbox" checked={item.done} ></input>
                             </li>)
                         })
                     } 
